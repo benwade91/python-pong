a6454a10227552df58ed0a7bbfab8ce33dc9c3c6
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-15 05:14:43 -0700

    added paddle and ball classes and paddle controls

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ screen = Screen()
 screen.setup(width=1000, height=800)
 screen.bgcolor('black')
 screen.title('Pong')
+screen.tracer(0)
 
-paddle = Paddle()
+right_player = Paddle()
+right_player.player_paddle('right')
+left_player = Paddle()
+left_player.player_paddle('left')
+
+screen.listen()
+screen.onkey(right_player.up, 'Up')
+screen.onkey(right_player.down, 'Down')
+screen.onkey(left_player.up, 'a')
+screen.onkey(left_player.down, 'z')
+
+game_on = True
+
+while game_on:
+    screen.update()
+
 
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -4,12 +4,19 @@ class Paddle(Turtle):
     def __init__(self):
         super().__init__()
         self.shape('square')
-        self.shapesize(5, 1, 15)
+        self.shapesize(1, 5, 15)
         self.color('white')
         self.penup()
+        self.setheading(90)
 
     def player_paddle(self, pos):
         if pos == 'right':
-            self.goto(350, 0)
+            self.goto(400, 0)
         elif pos == 'left':
-            self.goto(-350, 0)
+            self.goto(-400, 0)
+
+    def up(self):
+        self.forward(20)
+
+    def down(self):
+        self.backward(20)
